775ee328c17b3d0f11acc5db0a619fc8d647d307
atakocabas <atakocabas@gmail.com>
2023-10-22 15:13:06 +0300

    Hello World!

--- src/main/java/com/catan/app/AppApplication.java
+++ src/main/java/com/catan/app/AppApplication.java
@@ -2,9 +2,11 @@ package com.catan.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
