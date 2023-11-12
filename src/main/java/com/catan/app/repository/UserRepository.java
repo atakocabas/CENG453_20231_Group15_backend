71fb9d4da8234694a13a17595661a4016a7f7234
asrin <yilmaz.asrin@proton.me>
2023-11-12 16:41:48 +0300

    Removed findUserByUsernameAndPassword since, password check is done by the service layer and added updatePasswordAndSalt.

--- src/main/java/com/catan/app/repository/UserRepository.java
+++ src/main/java/com/catan/app/repository/UserRepository.java
@@ -2,17 +2,18 @@ package com.catan.app.repository;
 
 import com.catan.app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM User u WHERE u.username = ?1 AND u.password = ?2")
-    User findUserByUsernameAndPassword(String username, String password);
-
     @Query("SELECT u FROM User u WHERE u.username = ?1")
     User findUserByUsername(String username);
 
-    @Query("UPDATE User u SET u.password = ?2 WHERE u.username = ?1")
-    void updatePassword(String username, String newPassword);
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.hashedPassword = ?2, u.salt = ?3 WHERE u.username = ?1")
+    void updatePasswordAndSalt(String username, String hash, String salt);
 }
