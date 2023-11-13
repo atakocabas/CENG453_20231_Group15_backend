cfe27f507e9428f19f7a73bd5ca0c85d2bdf78fd
asrin <yilmaz.asrin@proton.me>
2023-11-13 23:24:39 +0300

    updated LeaderboardServiceTest to handle two dates that are very close

--- src/test/java/com/catan/app/service/LeaderboardServiceTest.java
+++ src/test/java/com/catan/app/service/LeaderboardServiceTest.java
@@ -4,6 +4,7 @@ import com.catan.app.entity.Leaderboard;
 import com.catan.app.entity.LeaderboardEntry;
 import com.catan.app.entity.Player;
 import com.catan.app.repository.LeaderboardRepository;
+import com.catan.app.util.IsCloseToDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -63,10 +64,9 @@ class LeaderboardServiceTest {
 
         // Mock the repository behavior
         when(leaderboardRepository.getDistinctPlayerIds()).thenReturn(Collections.singletonList(playerId));
-        when(leaderboardRepository.findScoresWithGivenDate(playerId, now.minusDays(7))).thenReturn(score);
 
-        Long mockPlayerId = leaderboardRepository.getDistinctPlayerIds().get(0);
-        Long mockScore = leaderboardRepository.findScoresWithGivenDate(playerId, now.minusDays(7));
+        LocalDateTime oneWeekAgo = LocalDateTime.now().minusDays(7);
+        when(leaderboardRepository.findScoresWithGivenDate(eq(playerId), argThat(new IsCloseToDateTime(oneWeekAgo, 60)))).thenReturn(score);
 
         // Call the method under test
         List<LeaderboardEntry> leaderboardEntries = leaderboardService.findWeeklyScores();
@@ -80,7 +80,7 @@ class LeaderboardServiceTest {
 
         // Verify interactions with the mock objects
         verify(leaderboardRepository).getDistinctPlayerIds();
-        verify(leaderboardRepository).findScoresWithGivenDate(playerId, now.minusDays(7));
+        verify(leaderboardRepository).findScoresWithGivenDate(eq(playerId), argThat(new IsCloseToDateTime(oneWeekAgo, 60)));
         verify(playerService).findUserById(playerId);
     }
 
@@ -95,13 +95,11 @@ class LeaderboardServiceTest {
         player.setPlayerName("testUser");
         when(playerService.findUserById(playerId)).thenReturn(player);
 
-        Leaderboard leaderboard = new Leaderboard();
-        leaderboard.setPlayerId(player);
-        leaderboard.setScore(score);
-
         // Mock the repository behavior
         when(leaderboardRepository.getDistinctPlayerIds()).thenReturn(Collections.singletonList(playerId));
-        when(leaderboardRepository.findScoresWithGivenDate(playerId, now.minusDays(30))).thenReturn(score);
+
+        LocalDateTime oneMonthAgo = LocalDateTime.now().minusDays(30);
+        when(leaderboardRepository.findScoresWithGivenDate(eq(playerId), argThat(new IsCloseToDateTime(oneMonthAgo, 60)))).thenReturn(score);
 
         // Call the method under test
         List<LeaderboardEntry> leaderboardEntries = leaderboardService.findMonthlyScores();
@@ -115,9 +113,40 @@ class LeaderboardServiceTest {
 
         // Verify interactions with the mock objects
         verify(leaderboardRepository).getDistinctPlayerIds();
-        verify(leaderboardRepository).findScoresWithGivenDate(playerId, now.minusDays(30));
+        verify(leaderboardRepository).findScoresWithGivenDate(eq(playerId), argThat(new IsCloseToDateTime(oneMonthAgo, 60)));
         verify(playerService).findUserById(playerId);
     }
 
-    // Similar tests can be written for findMonthlyScores and findAllTimeScores
+    @Test
+    void testFindAllTimeScores() {
+        Long playerId = 1L;
+        Long score = 100L;
+        LocalDateTime now = LocalDateTime.now();
+
+        Player player = new Player();
+        player.setId(playerId);
+        player.setPlayerName("testUser");
+        when(playerService.findUserById(playerId)).thenReturn(player);
+
+        // Mock the repository behavior
+        when(leaderboardRepository.getDistinctPlayerIds()).thenReturn(Collections.singletonList(playerId));
+
+        LocalDateTime hundredYearsAgo = LocalDateTime.now().minusYears(100);
+        when(leaderboardRepository.findScoresWithGivenDate(eq(playerId), argThat(new IsCloseToDateTime(hundredYearsAgo, 60)))).thenReturn(score);
+
+        // Call the method under test
+        List<LeaderboardEntry> leaderboardEntries = leaderboardService.findAllTimeScores();
+
+        // Assertions to verify the contents of the leaderboardEntries list
+        assertThat(leaderboardEntries).isNotNull();
+        assertThat(leaderboardEntries).hasSize(1);
+        LeaderboardEntry entry = leaderboardEntries.get(0);
+        assertThat(entry.getUsername()).isEqualTo(player.getPlayerName());
+        assertThat(entry.getScore()).isEqualTo(score);
+
+        // Verify interactions with the mock objects
+        verify(leaderboardRepository).getDistinctPlayerIds();
+        verify(leaderboardRepository).findScoresWithGivenDate(eq(playerId), argThat(new IsCloseToDateTime(hundredYearsAgo, 60)));
+        verify(playerService).findUserById(playerId);
+    }
 }
