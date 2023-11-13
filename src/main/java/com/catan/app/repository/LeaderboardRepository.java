55a1de116517905578d9e3e91835f798161804fb
atakocabas <atakocabas@gmail.com>
2023-11-13 19:11:21 +0300

    Simplified Queries to Given Last Time

--- src/main/java/com/catan/app/repository/LeaderboardRepository.java
+++ src/main/java/com/catan/app/repository/LeaderboardRepository.java
@@ -3,18 +3,16 @@ package com.catan.app.repository;
 import com.catan.app.entity.Leaderboard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface LeaderboardRepository extends JpaRepository<Leaderboard, Long> {
     @Query("SELECT DISTINCT l.playerId.id FROM Leaderboard l")
     List<Long> getDistinctPlayerIds();
-    @Query("SELECT SUM(l.score) FROM Leaderboard l WHERE l.date > CURRENT_DATE - 7 AND l.playerId.id IN ?1 GROUP BY l.playerId")
-    Long findWeeklyScores(Long playerId);
-    @Query("SELECT SUM(l.score) FROM Leaderboard l WHERE l.date > CURRENT_DATE - 30 AND l.playerId.id IN ?1 GROUP BY l.playerId")
-    Long findMonthlyScores(Long playerId);
-    @Query("SELECT SUM(l.score) FROM Leaderboard l WHERE l.playerId.id IN ?1 GROUP BY l.playerId")
-    Long findAllTimeScores(Long playerId);
+   @Query("SELECT SUM(l.score) FROM Leaderboard l WHERE l.playerId.id = :playerId AND l.date > :date GROUP BY l.playerId")
+    Long findScoresWithGivenDate(@Param("playerId") Long playerId, @Param("date") LocalDateTime date);
 }
