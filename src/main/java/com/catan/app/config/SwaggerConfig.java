dd11453f253a63fa949ce8c342e6c01f848cc0e6
asrin <yilmaz.asrin@proton.me>
2023-12-16 16:17:43 +0300

    Add Swagger configuration for API documentation

---
+++ src/main/java/com/catan/app/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.catan.app.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@SecurityScheme(
+        name = "JWT", // can be set to anything
+        type = SecuritySchemeType.HTTP,
+        scheme = "Bearer",
+        bearerFormat = "JWT"
+)
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
