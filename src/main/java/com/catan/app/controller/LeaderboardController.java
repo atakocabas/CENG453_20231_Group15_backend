acd66bd62bce8efaa1491668c0eaea1c98b7c1db
asrin <yilmaz.asrin@proton.me>
2023-12-16 16:19:13 +0300

    Update leaderboard endpoints to use RequestBody isntead of RequestParams

--- src/main/java/com/catan/app/controller/LeaderboardController.java
+++ src/main/java/com/catan/app/controller/LeaderboardController.java
@@ -1,13 +1,19 @@
 package com.catan.app.controller;
 
+import com.catan.app.dto.LeaderboardEntryRequest;
 import com.catan.app.entity.LeaderboardEntry;
 import com.catan.app.service.LeaderboardService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Enumeration;
 import java.util.List;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
+@SecurityRequirement(name = "JWT")
 @RestController
 @RequestMapping("api/v1/leaderboard")
 public class LeaderboardController {
@@ -20,11 +26,8 @@ public class LeaderboardController {
 
     @PostMapping("/add")
     @ResponseStatus(value = HttpStatus.CREATED)
-        public void addLeaderboardEntry(
-                @RequestParam("username") String username,
-                @RequestParam("score") Long score,
-                @RequestParam(value = "date", required = false) String date) {
-        leaderboardService.addLeaderboardEntry(username, score, date);
+    public void addLeaderboardEntry(@Validated @RequestBody LeaderboardEntryRequest request) {
+        leaderboardService.addLeaderboardEntry(request.username, request.score, request.date);
     }
 
     @GetMapping("/weekly")
